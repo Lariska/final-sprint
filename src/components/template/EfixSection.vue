4a735bf556c7a4790f0625e5e721a89392d7cb6e
klogpok <kloleg@gmail.com>
2017-07-04 19:14:36 +0300

    section added

--- src/components/template/EfixSection.vue
+++ src/components/template/EfixSection.vue
@@ -1,12 +1,51 @@
 <template>
-  <h3>up?</h3>
+  <section>
+    <div class="menu">
+      <a href=""><i class="fa fa-bars" aria-hidden="true"></i></a>
+      <ul class="nav">
+        <li><a href="">Link1</a></li>
+        <li><a href="">Link1</a></li>
+        <li><a href="">Link1</a></li>
+        <li><a href="">Link1</a></li>
+      </ul>
+    </div>
+  </section>
 </template>
 <<script>
 export default {
     name: 'efix-section',
-  
+
 }
 </script>
-<<style>
+<style>
+  header {
+    height: 60px;
+    background-color: #cccccc;
+  }
 
+  header .menu {
+    display: flex;
+    line-height: 60px;
+    font-size: 26px;
+    color: cornflowerblue;
+  }
+
+  header .menu a {
+    margin: 0 30px;
+  }
+
+  header .logo {
+    color: red;
+    font-size: 30px;
+  }
+
+  header .nav {
+    display: flex;
+    list-style: none;
+    margin: 0;
+  }
+
+  header .nav a {
+    text-decoration: none;
+  }
 </style>
