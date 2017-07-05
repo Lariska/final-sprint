21dee77059c55c867dbece63b366eaa3521cb678
Fima <FimaRozovsky@hotmail.com>
2017-07-05 19:53:33 +0300

    renamed according to Yarons question

---
+++ src/components/components/HeaderSection.vue
@@ -0,0 +1,53 @@
+<template>
+  <section>
+    <nav class="header">
+            <img class="logo" src="../../assets/wixer-logo.png" alt="Company name">
+        <ul class="text">
+            <h2 v-on:click="textClick">Hello</h2>
+            <h4>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam eaque illo quibusdam, consectetur eos facere cum perspiciatis
+                aperiam voluptas inventore nesciunt, assumenda nemo, maiores, deserunt ratione voluptatum sint expedita
+                labore!
+            </h4>
+        </ul>
+    </nav>
+  </section>
+</template>
+
+
+
+<script>
+    import $ from 'jquery';
+    export default {
+      name: 'header-section',
+      methods: {
+        textClick: function (event) {
+            var el = event.target;
+            var text = event.target.textContent;
+            $(el).text("");
+            $(el).append("<input value='" + text + "'/>");
+            // $(el).clear();
+        }
+      },
+    }
+  </script>
+
+
+
+  <style scoped>
+    .header{
+        display: flex;
+        align-content: center;
+        justify-content: center;
+        background-color: #CED3DC;
+    }
+    .logo{
+        margin-left: 10px;
+    }
+    .text{
+        display: flex;
+        align-content: center;
+        justify-content: center;
+    }
+  </style>
+  
---
+++ src/components/components/NavBarSection.vue
@@ -0,0 +1,56 @@
+<template>
+  <section>
+    <div class="menu">
+      <a href=""><i class="fa fa-bars" aria-hidden="true"></i></a>
+      <ul class="nav">
+        <li><a href="">Link1</a></li>
+        <li><a href="">Link1</a></li>
+        <li><a href="">Link1</a></li>
+        <li><a href="">Link1</a></li>
+      </ul>
+    </div>
+    <header-section></header-section>
+  </section>
+</template>
+<script>
+// import HeaderSection from './HeaderSection'
+export default {
+    name: 'nav-bar-section',
+    // components: {
+    //   "header-section": HeaderSection
+    // }
+}
+</script>
+<style scoped>
+  section {
+    height: 60px;
+    background-color: #ccc;
+    border: 1px solid #c1e2b3;
+  }
+
+  section .menu {
+    display: flex;
+    line-height: 60px;
+    font-size: 26px;
+    color: cornflowerblue;
+  }
+
+  section .menu a {
+    margin: 0 30px;
+  }
+
+  section .logo {
+    color: red;
+    font-size: 30px;
+  }
+
+  section .nav {
+    display: flex;
+    list-style: none;
+    margin: 0;
+  }
+
+  section .nav a {
+    text-decoration: none;
+  }
+</style>
