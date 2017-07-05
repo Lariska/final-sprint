eae20309cd303d4fd6d5706b73f121713236f08a
klogpok <kloleg@gmail.com>
2017-07-05 09:59:22 +0300

    revert

--- src/components/EfixList.vue
+++ src/components/EfixList.vue
@@ -1,46 +1,48 @@
 <template>
-    <section>
-        <h2>Whats</h2>
-        <nav-bar-section v-for="section in sectionCount" :key="section"></nav-bar-section>
-        <div>
-            <button @click="updateCount">+</button>
-        </div>
-        <div id="modal"></div>
-    </section>
+  <section>
+    <h2>Whats</h2>
+    <nav-bar-section v-for="section in sectionCount" :key="section"></nav-bar-section>
+    <div>
+      <button @click="updateCount">+</button>
+    </div>
+    <choice-btn></choice-btn>
+    <div id="modal"></div>
+  </section>
 </template>
 <script>
-import NavBarSection from './template/NavBarSection'
-import FooterSection from './template/FooterSection'
-import MainSection from './template/MainSection'
-export default {
-  name: 'efix-list',
-  components: {NavBarSection, FooterSection, MainSection},
-  data(){
-      return{
+  import NavBarSection from './template/NavBarSection'
+  import FooterSection from './template/FooterSection'
+  import MainSection from './template/MainSection'
+  export default {
+    name: 'efix-list',
+    components: {NavBarSection, FooterSection, MainSection, ChoiceBtn},
+    data(){
+      return {
         sectionCount: 0
       }
-  },
-  methods: {
+    },
+    methods: {
       updateCount(){
 
         this.sectionCount += 1;
       }
+    }
   }
-}
 </script>
 
 <style scoped>
-    div{
-        display:flex;
-        justify-content: flex-end;
-    }
-    #modal{
-        background-color: red;
-        height: 150px;
-        width: 50%;
-        position: absolute;
-        left: 25%;
-        display:none;
-    }
+  div {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  #modal {
+    background-color: red;
+    height: 150px;
+    width: 50%;
+    position: absolute;
+    left: 25%;
+    display: none;
+  }
 </style>
 
