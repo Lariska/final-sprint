90ab320af111f3ec95cf174b9eb995e16c9d4155
Fima <FimaRozovsky@hotmail.com>
2017-07-06 11:27:17 +0300

    fixed merging issues

---
+++ src/components/editor/CloseBtn.vue
@@ -0,0 +1,34 @@
+<<template>
+<div @click="close">x</div>  
+</template>
+<<script>
+export default {
+  name: 'close-btn',
+  created() {
+      console.log("created");
+  },
+  methods: {
+      close(){
+          console.log("click");
+      }
+  }
+}
+</script>
+<<style scoped>
+    
+    div{
+        color:red;
+        font-size:2em;
+        background-color: gray;
+        opacity:0.5;
+        height:10%;
+        -webkit-transition: 0.2s;
+        -moz-transition: 0.2s;
+        transition: 0.2s;
+    }
+    div:hover{
+        cursor:pointer;
+        opacity:1;
+    }
+</style>
+
---
+++ src/components/editor/Toolbar.vue
@@ -0,0 +1,11 @@
+<<template>
+<div><i class="el-icon-iconName"></i></div>  
+</template>
+<<script>
+export default {
+  name: 'tool-bar'
+}
+</script>
+<<style>
+
+</style>
